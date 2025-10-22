a97c5825ff6171decbac8c2c62b1011774385921
Anastasov <anastasovdavid@gmail.com>
2025-10-22 07:41:33 +0200

    implemented database in configuration

--- src/main/kotlin/config/DatabaseFactory.kt
+++ src/main/kotlin/config/DatabaseFactory.kt
@@ -1,15 +1,30 @@
 package com.config
 
 import com.model.Users
+import io.ktor.server.application.ApplicationEnvironment
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
-    fun init() {
+/*    fun init() {
         Database.connect("jdbc:postgresql://localhost:5432/ktor_crud", driver = "org.postgresql.Driver", user = "user", password = "admin")
         transaction {
             SchemaUtils.create(Users);
+        }
+    }*/
+
+    fun init(environment: ApplicationEnvironment) {
+        val config = environment.config.config("database")
+        val url = config.property("url").getString()
+        val user = config.property("user").getString()
+        val password = config.property("password").getString()
+        val driver = config.property("driver").getString()
+
+        Database.connect(url, driver, user, password)
+
+        transaction {
+            SchemaUtils.create(Users)
         }
     }
 }
