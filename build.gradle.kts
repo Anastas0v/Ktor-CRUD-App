4de32351809b2c3f24f1035069eb44226c8da809
Anastasov <anastasovdavid@gmail.com>
2025-10-22 21:03:07 +0200

    downgrade versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+val kotlin_version: String by project
+val logback_version: String by project
+
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.ktor)
+    kotlin("jvm") version "2.2.20"
+    id("io.ktor.plugin") version "3.3.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0"
 }
 
 group = "com"
@@ -11,12 +15,14 @@ application {
 }
 
 dependencies {
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.netty)
-    implementation(libs.logback.classic)
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.config.yaml)
-    implementation(libs.logback.classic)
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-config-yaml")
+
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.12")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.12")
 
     // --- Database ---
     implementation("org.jetbrains.exposed:exposed-core:0.50.1")
@@ -25,11 +31,6 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-java-time:0.50.1")
     implementation("org.postgresql:postgresql:42.7.4")
 
-    // --- JSON Serialization ---
-    implementation("io.ktor:ktor-server-content-negotiation")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
-
-    // --- Tests ---
-    testImplementation(libs.ktor.server.test.host)
-    testImplementation(libs.kotlin.test.junit)
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
